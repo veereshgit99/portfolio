ed880c6379ab18ac12828bc9588da6e32506066a
Veeresh Koliwad <157433566+veereshgit99@users.noreply.github.com>
2024-11-22 01:49:36 -0700

    Deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'Veeresh Shanmukha Koliwad',
-        short_name: 'Veeresh Shanmukha Koliwad',
+        short_name: 'portfolio',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
